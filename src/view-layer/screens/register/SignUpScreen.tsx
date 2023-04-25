fa699dd4ad81e46891a2dfd441b20f722c8a563d
MatheusVSouza <pitoko.vm@gmail.com>
2023-04-25 14:45:54 -0300

    fix: prettier/lint

--- src/view-layer/screens/register/SignUpScreen.tsx
+++ src/view-layer/screens/register/SignUpScreen.tsx
@@ -1,5 +1,12 @@
 import { View, StyleSheet, ScrollView } from "react-native";
-import { IconButton, MD3Theme, Text, useTheme, TextInput , Button} from "react-native-paper";
+import {
+  IconButton,
+  MD3Theme,
+  Text,
+  useTheme,
+  TextInput,
+  Button,
+} from "react-native-paper";
 import { Appbar } from "../../shared/components/Appbar";
 import { useMemo } from "react";
 
@@ -8,100 +15,73 @@ export function SignUpScreen() {
   const styles = useMemo(() => createStyles(theme), [theme]);
   return (
     <>
-      <Appbar title="Cadastro Pessoal"/>
+      <Appbar title="Cadastro Pessoal" />
       <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.rectangle}>
-        <Text style={styles.description}>
-        As informações preenchidas serão divulgadas
-        apenas para a pessoa com a qual você realizar
-        o processo de adoção e/ou apadrinhamento,
-        após a formalização do processo.
-        </Text>
-        </View>
-        <View style={styles.subtitleContainer}>
-          <Text style={styles.subtitle}>
-            INFORMAÇÕES PESSOAIS
-          </Text>
-          <TextInput
-            placeholder='Nome completo'
-            dense
-            style={styles.inputStyle}/>
+        <View style={styles.container}>
+          <View style={styles.rectangle}>
+            <Text style={styles.description}>
+              As informações preenchidas serão divulgadas apenas para a pessoa
+              com a qual você realizar o processo de adoção e/ou apadrinhamento,
+              após a formalização do processo.
+            </Text>
+          </View>
+          <View style={styles.subtitleContainer}>
+            <Text style={styles.subtitle}>INFORMAÇÕES PESSOAIS</Text>
+            <TextInput
+              placeholder="Nome completo"
+              dense
+              style={styles.inputStyle}
+            />
 
-          <TextInput
-            placeholder='Idade'
-            dense
-            style={styles.inputStyle}/>
+            <TextInput placeholder="Idade" dense style={styles.inputStyle} />
 
-          <TextInput
-            placeholder='E-mail'
-            dense
-            style={styles.inputStyle}/>
+            <TextInput placeholder="E-mail" dense style={styles.inputStyle} />
 
-          <TextInput
-            placeholder='Estado'
-            dense
-            style={styles.inputStyle}/>
+            <TextInput placeholder="Estado" dense style={styles.inputStyle} />
 
-          <TextInput
-            placeholder='Cidade'
-            dense
-            style={styles.inputStyle}/>
+            <TextInput placeholder="Cidade" dense style={styles.inputStyle} />
 
-          <TextInput
-            placeholder='Endereço'
-            dense
-            style={styles.inputStyle}/>
+            <TextInput placeholder="Endereço" dense style={styles.inputStyle} />
 
-          <TextInput
-            placeholder='Telefone'
-            dense
-            style={styles.inputStyle}/>
-          
-          <Text style={styles.subtitle}>
-            INFORMAÇÕES DE PERFIL
-          </Text>
+            <TextInput placeholder="Telefone" dense style={styles.inputStyle} />
 
-          <TextInput
-            placeholder='Nome de usuário'
-            dense
-            style={styles.inputStyle}/>
+            <Text style={styles.subtitle}>INFORMAÇÕES DE PERFIL</Text>
 
-          <TextInput
-            placeholder='Senha'
-            dense
-            style={styles.inputStyle}/>
+            <TextInput
+              placeholder="Nome de usuário"
+              dense
+              style={styles.inputStyle}
+            />
 
-          <TextInput
-            placeholder='Confirmação de senha'
-            dense
-            style={styles.inputStyle}/>
-          
-          <Text style={styles.subtitle}>
-            FOTO DE PERFIL
-          </Text>
-        </View>
-        <View style={styles.pictureRectangle}>
-          <IconButton
-            icon="plus-circle-outline"
-            size={24} 
-            onPress={()=>{}}
-            style={styles.controlPointIcon}
-            iconColor="#757575"
-          />
-          <Text style={styles.pictureText}>
-            adicionar foto
-          </Text>
-        </View>
-        <Button
+            <TextInput placeholder="Senha" dense style={styles.inputStyle} />
+
+            <TextInput
+              placeholder="Confirmação de senha"
+              dense
+              style={styles.inputStyle}
+            />
+
+            <Text style={styles.subtitle}>FOTO DE PERFIL</Text>
+          </View>
+          <View style={styles.pictureRectangle}>
+            <IconButton
+              icon="plus-circle-outline"
+              size={24}
+              onPress={() => {}}
+              style={styles.controlPointIcon}
+              iconColor="#757575"
+            />
+            <Text style={styles.pictureText}>adicionar foto</Text>
+          </View>
+          <Button
             mode="contained"
             style={styles.buttonContainer}
             textColor="#434343"
-            onPress={()=>{}}
+            onPress={() => {}}
           >
             FAZER CADASTRO
           </Button>
-      </View>
+        </View>
       </ScrollView>
     </>
   );
@@ -139,22 +119,22 @@ const createStyles = (theme: MD3Theme) =>
 
     subtitleContainer: {
       paddingHorizontal: 28,
-      paddingBottom: 32
+      paddingBottom: 32,
     },
 
-    subtitle:{
+    subtitle: {
       paddingTop: 32,
       color: theme.colors.primary,
-      fontSize: 14
+      fontSize: 14,
     },
 
-    inputStyle:{
+    inputStyle: {
       ...baseStyle.input,
       color: "#bdbdbd",
       fontSize: 14,
-      paddingTop: 36
+      paddingTop: 36,
     },
-    
+
     pictureRectangle: {
       marginTop: 16,
       width: 128,
@@ -162,22 +142,22 @@ const createStyles = (theme: MD3Theme) =>
       backgroundColor: "#e6e7e7",
       paddingTop: 16,
       borderRadius: 2,
-      alignSelf: "center"
+      alignSelf: "center",
     },
 
     controlPointIcon: {
-      alignSelf:"center"
+      alignSelf: "center",
     },
 
     pictureText: {
-      color:"#757575",
-      textAlign:"center"
+      color: "#757575",
+      textAlign: "center",
     },
 
     buttonContainer: {
       alignSelf: "center",
       marginTop: 52,
       width: 232,
-      marginBottom: 32
+      marginBottom: 32,
     },
-  })
+  });
