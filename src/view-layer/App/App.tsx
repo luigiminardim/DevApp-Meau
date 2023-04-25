7a083c1f42aaae1c1386c607e4c6a44d6bbcb6c9
Théo Gallo <gallotheo@gmail.com>
2023-04-25 20:57:43 -0300

    feat: DM-6 Create login error screen (Ops!) (#5)

--- src/view-layer/App/App.tsx
+++ src/view-layer/App/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "../contexts/ThemeContext";
 // import { LoginScreen } from "../screens";
 // import { IntroductionScreen } from "../screens/introduction";
-import { SignUpScreen } from "../screens/register";
+// import { SignUpScreen } from "../screens/register";
+import { OopsScreen } from "../screens/requireLogin/oopsScreen";
 
 export function App() {
   return (
     <ThemeProvider>
+      <OopsScreen />
       {/* <IntroductionScreen /> */}
       {/* <LoginScreen /> */}
-      <SignUpScreen />
+      {/* <SignUpScreen /> */}
     </ThemeProvider>
   );
 }
