1cba2879f38473ef32dd6ad19039ca6cbe682916
Luigi Minardi <luigiminardim@gmail.com>
2023-06-01 19:45:55 -0300

    refactor: :recycle: Use just one method in use cases to signup.

--- src/gateway-layer/firebase-module/user-module/gates/signUpGate.ts
+++ src/gateway-layer/firebase-module/user-module/gates/signUpGate.ts
@@ -1,15 +1,46 @@
-import { addDoc, Firestore, collection } from "firebase/firestore";
+import {
+  Auth as FirebaseAuth,
+  createUserWithEmailAndPassword,
+} from "@firebase/auth";
+import {
+  addDoc,
+  Firestore as FirebaseDb,
+  collection,
+} from "firebase/firestore";
 import { SignUpUsecase } from "../../../../core-layer/user-module";
+import { UserData } from "../dto/UserData";
 
 export class SignUpGate implements SignUpUsecase {
-  constructor(private firebaseDb: Firestore) {}
+  constructor(
+    private firebaseAuth: FirebaseAuth,
+    private firebaseDb: FirebaseDb
+  ) {}
   signUpWithPassword: SignUpUsecase["signUpWithPassword"] = async (param) => {
     try {
-      const dbRef = collection(this.firebaseDb, "users");
-      const { id } = await addDoc(dbRef, param);
-      return { type: "success", userId: id };
+      const createAuthResult = await createUserWithEmailAndPassword(
+        this.firebaseAuth,
+        param.email,
+        param.password
+      );
+      const userData: UserData = {
+        id: createAuthResult.user.uid,
+        email: param.email,
+        name: param.name,
+        age: param.age,
+        state: param.state,
+        city: param.city,
+        address: param.address,
+        phone: param.phone,
+      };
+      try {
+        const dbRef = collection(this.firebaseDb, "users");
+        const { id } = await addDoc(dbRef, userData);
+        return { type: "success", userId: id };
+      } catch (err) {
+        return { type: "error", error: String(err) };
+      }
     } catch (err) {
-      return { type: "error", error: err as string };
+      return { type: "error", error: String(err) };
     }
   };
 }
