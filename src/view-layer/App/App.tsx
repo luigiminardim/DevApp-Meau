7b0b9cd143ea06837f47096c466daa4554d9786c
Luigi Minardi <luigiminardim@gmail.com>
2023-06-03 19:39:20 -0300

    refactor: :recycle: #DM-39 Create UserContext

--- src/view-layer/App/App.tsx
+++ src/view-layer/App/App.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView, ScrollView } from "react-native";
 import { CoreLayer } from "../../core-layer";
 import { CoreLayerProvider } from "../contexts/CoreLayerContext";
+import { UserProvider } from "../contexts/UserContext";
 import { ThemeProvider } from "../contexts/ThemeContext";
 import { LoginScreen } from "../screens";
 import { StyleSheet } from "react-native";
@@ -16,18 +17,20 @@ export type AppProps = {
 export function App({ coreLayer }: AppProps) {
   return (
     <CoreLayerProvider coreLayer={coreLayer}>
-      <ThemeProvider>
-        <SafeAreaView style={styles.safeArea}>
-          <ScrollView>
-            <SignUpScreen />
-            <LoginScreen />
-            {/* <OopsScreen /> */}
-            {/* <IntroductionScreen /> */}
-            {/* <SignUpScreen /> */}
-            {/* <RegisterAnimalScreen /> */}
-          </ScrollView>
-        </SafeAreaView>
-      </ThemeProvider>
+      <UserProvider>
+        <ThemeProvider>
+          <SafeAreaView style={styles.safeArea}>
+            <ScrollView>
+              <SignUpScreen />
+              <LoginScreen />
+              {/* <OopsScreen /> */}
+              {/* <IntroductionScreen /> */}
+              {/* <SignUpScreen /> */}
+              {/* <RegisterAnimalScreen /> */}
+            </ScrollView>
+          </SafeAreaView>
+        </ThemeProvider>
+      </UserProvider>
     </CoreLayerProvider>
   );
 }
